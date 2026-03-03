b003f9c4c839d4bbad95af738485b550b827dc85
tj-commits <thefoxjones@gmail.com>
2026-03-03 16:50:23 -0600

    chore: update code to make ESLint pass

--- index.js
+++ index.js
@@ -1,1 +1,2 @@
-module.exports = (x, y) => x == y
+/* eslint-disable-next-line eqeqeq, 10x-engineering/no-operators, unicorn/no-anonymous-default-export */
+module.exports = (value1, value2) => value1 == value2
